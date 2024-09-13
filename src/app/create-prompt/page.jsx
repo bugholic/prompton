09b15b4226746aff948e3da84005cecc9882abfc
bugholic <masterav2001@gmail.com>
2024-09-13 10:38:11 +0530

    new functions added

--- src/app/create-prompt/page.jsx
+++ src/app/create-prompt/page.jsx
@@ -3,7 +3,7 @@
 import { useSession } from "next-auth/react"
 import { useRouter } from "next/router"
 import { useState } from "react"
-import Form from "../components/Form"
+import Form from "../components/Form" 
 
 
 const CreatePrompt = () => {
@@ -17,9 +17,20 @@ const createPrompt = async (e)=>{
   e.preventDefault();  
   setSubmitting(true);
   try {
-    
+    const response = await fetch("/api/prompt/new",{
+      method: 'POST',
+      body: JSON.stringify({
+        prompt: post.prompt,
+        userId: session?.user.id,
+        tag: post.tag
+      })
+    })
+    if(response.ok){
+      router.push('/');
+    }
   } catch (error) {
-    
+    console.log(error)
+    setSubmitting(false)
   }
   
 }
